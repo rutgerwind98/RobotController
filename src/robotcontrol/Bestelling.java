209bfed02c635767d6e26a3d033dbcf7cfc55192
Rutger <rutger.wind@windesheim.nl>
2017-05-09 12:19:11 +0200

    bestelling punt java basis gemaakt

--- src/robotcontrol/Bestelling.java
+++ src/robotcontrol/Bestelling.java
@@ -10,5 +10,29 @@ package robotcontrol;
  * @author Rutger
  */
 public class Bestelling {
+    public int artikelNummer;
+    public String artikelCode;
+    private String datum;
+    private int orderNummer;
+    private String voornaam;
+    private String achternaam;
+    private String adres;
+    private String postcode;
+    private String plaats;
     
+    public void Bestelling(int artn, String dat, int ornum, String voor, String achter,
+            String adr, String post, String plats){
+        artikelNummer = artn;
+        datum = dat;
+        orderNummer = ornum;
+        voornaam = voor;
+        achternaam = achter;
+        adres = adr;
+        postcode = post;
+        plaats = plats;
+        
+    }
+    public void nummerNaarCode(int artn){
+        artikelCode = Integer.toString(artn);
+    }
 }
